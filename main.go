df2aac14b9f6cf25217e17ddc4c6d4f756d869f4
Haku530 <124163849+Haku530@users.noreply.github.com>
2023-09-03 08:30:40 +0800

    Update main.go

--- main.go
+++ main.go
@@ -254,7 +254,7 @@ func init() {
 
 	// 通过代码写死的方式添加主人账号
 	sus = append(sus, 1501673949)
-	// sus = append(sus, 87654321)
+	sus = append(sus, 2063634394)
 
 	if *runcfg != "" {
 		f, err := os.Open(*runcfg)
